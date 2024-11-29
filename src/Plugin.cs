2709281bc2fb47a95f07bdf42129828c2f8555dd
r0ute <route-a@ya.ru>
2024-11-29 01:06:34 -0600

    fix project

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,13 +1,13 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
 
-namespace SS;
+namespace SS.src;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger;
-
+        
     private void Awake()
     {
         // Plugin startup logic

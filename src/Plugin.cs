63d4b59e727d3254b8fe6f2be8aea341a28738e2
r0ute <route-a@ya.ru>
2024-11-29 08:20:39 -0600

    OnPricingItemSetup

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
 using MyBox;
 using TMPro;
+using UnityEngine;
 
 namespace SS.src;
 
@@ -16,6 +18,7 @@ public class Plugin : BaseUnityPlugin
     {
         // Plugin startup logic
         Logger = base.Logger;
+
         Harmony harmony = new(MyPluginInfo.PLUGIN_GUID);
         harmony.PatchAll(typeof(Patches));
 
@@ -25,25 +28,27 @@ public class Plugin : BaseUnityPlugin
     class Patches
     {
 
+
         [HarmonyPatch(typeof(PricingItem), nameof(PricingItem.Setup))]
         [HarmonyPostfix]
-        static void OnSetup(Pricing data, ref PricingItem __instance, ref bool __runOriginal)
+        static void OnPricingItemSetup(Pricing data, ref PricingItem __instance)
         {
-            var product = Singleton<IDManager>.Instance.ProductSO(data.ProductID);
-            var purchaseChance = Singleton<PriceEvaluationManager>.Instance.PurchaseChance(data.ProductID);
+            var avgCostText = Traverse.Create(__instance).Field("m_AvgCostText").GetValue() as TMP_Text;
+            avgCostText.text = string.Format("{0} / {1}",
+                data.AvgCost.ToMoneyText(avgCostText.fontSize),
+                Singleton<PriceEvaluationManager>.Instance.PurchaseChance(data.ProductID)
+                    .ToString("0.##\\%"));
 
+
+            var marketPriceText = Traverse.Create(__instance).Field("m_MarketPriceText").GetValue() as TMP_Text;
             var currentCost = Singleton<PriceManager>.Instance.CurrentCost(data.ProductID);
+            var product = Singleton<IDManager>.Instance.ProductSO(data.ProductID);
             var maxCost = (float)Math.Round(currentCost + currentCost * product.MaxProfitRate / 100f, 2);
-
-            var avgCost = Traverse.Create(__instance).Field("m_AvgCostText").GetValue() as TMP_Text;
-            avgCost.text = purchaseChance.ToString("0.##\\%");
-
-            var mktPrice = Traverse.Create(__instance).Field("m_MarketPriceText").GetValue() as TMP_Text;
-            mktPrice.text = string.Format("{0}</size>..{1}", data.MarketPrice.ToMoneyText(mktPrice.fontSize),
-                maxCost.ToMoneyText(mktPrice.fontSize));
+            marketPriceText.text = string.Format("{0}</size>..{1}",
+                data.MarketPrice.ToMoneyText(marketPriceText.fontSize),
+                maxCost.ToMoneyText(marketPriceText.fontSize));
 
         }
-
 
 
     }
